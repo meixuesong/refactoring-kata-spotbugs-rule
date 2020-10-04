7ae9791a769ed1af7e1ac7d324e8b31b7a3df0c7
Mei Xuesong <meixuesong@gmail.com>
2020-10-04 22:11:02 +0800

    init project

---
+++ src/test/java/refactoring/kata/rule/BadCase.java
@@ -0,0 +1,7 @@
+package refactoring.kata.rule;
+
+class BadCase {
+    void method() {
+        System.out.println("Hello SpotBugs!");
+    }
+}
---
+++ src/test/java/refactoring/kata/rule/GoodCase.java
@@ -0,0 +1,7 @@
+package refactoring.kata.rule;
+
+class GoodCase {
+    void method() {
+        System.err.println("Hello SpotBugs!");
+    }
+}
